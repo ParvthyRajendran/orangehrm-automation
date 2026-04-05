8d2dcb35e825530a701e76e6e8949da34147f166
Parvathy Rajendran <parvathy.rajendran@s2021.tu-chemnitz.de>
2026-04-05 17:46:05 +0200

    feat: headless Chrome in CI, restore EmployeePage to working state

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,6 +3,7 @@ package pages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
@@ -12,8 +13,18 @@ public class BasePage {
 
     public static void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+        driver = new ChromeDriver(options);
+        if (System.getenv("CI") == null) {
+            driver.manage().window().maximize();
+        }
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
